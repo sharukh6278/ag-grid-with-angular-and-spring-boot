af38e97c1f1fb473a75379eedfbb6163c1544740
sharukh <sharukh6278@gmail.com>
2023-01-25 10:31:59 +0530

    ag-grid with angular and spring boot with sorting and filtering

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,91 @@
+
+a
+1Spring Boot/.mvn/wrapper/maven-wrapper.properties,b\0\b0a2767c621e028b25526529f0735322330a3cc9
+d
+4Angular Demo/src/app/service/app/app.service.spec.ts,b\9\b94158bab8cf19aa47a0d1d69896f5b4378f0661
+_
+/Angular Demo/src/app/service/app/app.service.ts,a\2\a29c569a03b12dff29618dca00e0dd6f31862d54
+\
+,Spring Boot/src/main/resources/data/data.csv,1\3\1391c1467a911d1f788b1e74d653dd53f93e0021
+Z
+*Angular Demo/src/app/app-routing.module.ts,6\8\68aec2c3427a3fcea9ebfea7aa8150066913d6f2
+V
+&Angular Demo/src/app/app.component.css,5\2\522f62eb45e59a8d50f778fce9aa4ca5e664cb31
+P
+ Angular Demo/src/assets/.gitkeep,3\f\3f4e9ae382d09d8282f06db643933b9c87585903
+u
+ESpring Boot/src/main/java/com/ag/grid/demo/AgGridDemoApplication.java,3\a\3ac7234201f8ed86a366b2e56c2837f22abb4c54
+a
+1Angular Demo/src/environments/environment.prod.ts,a\3\a316c0da10820df4f506ff8fefc7278bf2037a37
+W
+'Angular Demo/src/app/app.component.html,5\3\532ec1116c0a51f7e62a7d39f7f24c26ffeda936
+Z
+*Angular Demo/src/app/app.component.spec.ts,d\e\de44a5dc0241495302364360e8093fb17bb560c5
+U
+%Angular Demo/src/app/app.component.ts,0\a\0aa27fad76a6bb9faf7981e8ddddd8464c170f80
+R
+"Angular Demo/src/app/app.module.ts,b\0\b0c969813aa8fa65c81c5b6e8eaa8e762e962310
+\
+,Angular Demo/src/environments/environment.ts,3\4\3402b224c4c159db1a4bc25f6e083f866f6ef62d
+z
+JSpring Boot/src/test/java/com/ag/grid/demo/AgGridDemoApplicationTests.java,2\1\2132aaa9986bbf179d6347be6b380efddec472c0
+u
+ESpring Boot/src/main/java/com/ag/grid/demo/model/CompanyResponse.java,3\c\3c11cba052d5b6ed800cc62b3af3d163583e3e0d
+q
+ASpring Boot/src/main/java/com/ag/grid/demo/model/FilterModel.java,0\6\067bd5bba67484e8645df105ddb93f83418d1af0
+~
+NSpring Boot/src/main/java/com/ag/grid/demo/model/RequestWithFilterAndSort.java,b\8\b84489cba10df86588391e31e5e113066d6e5bac
+€
+PSpring Boot/src/main/java/com/ag/grid/demo/controller/CompanyRestController.java,f\8\f8bac58fa6c63345def60a3addcdafdf1ea72feb
+l
+<Spring Boot/src/main/java/com/ag/grid/demo/pojo/Company.java,c\a\cab8b6f1d015b2a34369726602e923e643cef9f2
+J
+Angular Demo/.editorconfig,1\a\1a3ad8814898218032e4f9a0ad86ee27040a9034
+I
+Angular Demo/angular.json,7\4\74bfe426255dff71e5a0eea787f287a4ef0b6dc9
+|
+LSpring Boot/src/main/java/com/ag/grid/demo/repository/CompanyRepository.java,5\c\5c5e27cec3d99fde3aaca1e4989912508cb29512
+L
+Angular Demo/.browserslistrc,8\a\8afca31065c0a9008e6287887930964596e6465e
+F
+Angular Demo/README.md,2\2\22de723d03a8edb9ef8b0a012c141d7444b6c78c
+N
+Angular Demo/tsconfig.app.json,f\b\fbcb587d13ae393ec130c9c2dcf23204d042b516
+J
+Angular Demo/tsconfig.json,6\8\682c1d9a54eb1260c3c8c12ea4f7100a0ebd2e99
+J
+Angular Demo/karma.conf.js,9\7\97bc4b857edfd2388c585a6fe2fa65de8348ce1c
+N
+Angular Demo/package-lock.json,b\3\b30f1a5cc53ac31dc6613960f048fea8a3360319
+I
+Angular Demo/package.json,5\6\56fdfd235b54d022e8b8ad1c063ee972acf91885
+K
+Angular Demo/src/index.html,2\5\25e37b4f81ac91f87be98384ef1804c851efaf7f
+H
+Angular Demo/src/main.ts,3\2\3225e0ca2740b742f5293be7cca1f7d419932df1
+M
+Angular Demo/src/polyfills.ts,7\9\79fe59e49932332d3e8a670b5ad521812f37728b
+O
+Angular Demo/tsconfig.spec.json,4\2\42d0e018ff825cea6f0b1b63ed8056941bafd252
+O
+Angular Demo/.vscode/tasks.json,9\d\9d02d73c847f304d4581f92741851d7af7191d25
+F
+Spring Boot/.gitignore,7\2\72fb045e9ad342a9062a806b208afacef8e79417
+K
+Angular Demo/src/styles.css,3\9\396de6d4377d43f502a4d5767932374ad0dd4d33
+H
+Angular Demo/src/test.ts,9\b\9bae22f38106b8dcd4f2be58a429a4fbd1ea0b5f
+T
+$Angular Demo/.vscode/extensions.json,6\5\6543d8fc4f08118eab19461bbcea9c55c54d2bbf
+P
+ Angular Demo/.vscode/launch.json,b\9\b9569ef949d730122cd584744b17d4af3567717e
+D
+Spring Boot/mvnw.cmd,c\e\cea44bba7a73594154376eabc8903b5a3e4d30c6
+C
+Spring Boot/pom.xml,b\6\b6b289d60b8acf7586c7cf8a59b987189065a8d7
+@
+Spring Boot/mvnw,3\0\303638c4eecf2ff1f8f6316315fea9d8c40c44c8
+f
+6Angular Demo/src/app/model/request-with-sort-filter.ts,2\e\2ead86f5704b6bf12d823efb9fbebb62029a861f
+G
+Angular Demo/.gitignore,a\f\af55bcddc4ef5074ff2740d551bab677b66c0df8
--- Spring Boot/src/main/java/com/ag/grid/demo/repository/CompanyRepositoryCustom.java
+++ Spring Boot/src/main/java/com/ag/grid/demo/repository/CompanyRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.ag.grid.demo.repository;
 
 import com.ag.grid.demo.model.CompanyResponse;
+import com.ag.grid.demo.model.FilterModel;
 import com.ag.grid.demo.model.RequestWithFilterAndSort;
 import com.ag.grid.demo.pojo.Company;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,11 @@ import org.springframework.stereotype.Component;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Component
@@ -34,12 +38,71 @@ public class CompanyRepositoryCustom {
                 companyRoot.get("leave")
                 );
 
-        Optional.ofNullable(requestWithFilterAndSort.getColId()).isPresent(colId);
+        if(requestWithFilterAndSort.getColId()!=null && requestWithFilterAndSort.getSort().equals("desc")){
+            companyResponseCriteriaQuery.orderBy(criteriaBuilder.desc(companyRoot.get(requestWithFilterAndSort.getColId())));
+        }
+        else if(requestWithFilterAndSort.getColId()!=null && requestWithFilterAndSort.getSort().equals("asc")){
+            companyResponseCriteriaQuery.orderBy(criteriaBuilder.asc(companyRoot.get(requestWithFilterAndSort.getColId())));
+        }
+
+        List<Predicate> predicateList=new ArrayList<>();
+        predicateList.addAll(getFilter(criteriaBuilder,predicateList,companyRoot,requestWithFilterAndSort));
+        Predicate predicateArray[]=new Predicate[predicateList.size()];
+
+        companyResponseCriteriaQuery.where(criteriaBuilder.and(predicateList.toArray(predicateArray)));
+
         List<CompanyResponse> companyResponseList=entityManager.createQuery(companyResponseCriteriaQuery).getResultList();
         List<CompanyResponse> companyResponses=entityManager.createQuery(companyResponseCriteriaQuery).
                 setFirstResult((int) pageable.getOffset()).
                 setMaxResults(pageable.getPageSize()).
                 getResultList();
         return new PageImpl<>(companyResponses,pageable,companyResponseList.size());
+    }
+
+    private List<Predicate> getFilter(CriteriaBuilder criteriaBuilder,List<Predicate> predicateList,Root<Company> companyRoot,RequestWithFilterAndSort requestWithFilterAndSort){
+        Optional.ofNullable(requestWithFilterAndSort.getFilterModel()).ifPresent(filterModelMap->{
+            for(Map.Entry<String, FilterModel> filterModelEntry:filterModelMap.entrySet()){
+
+                String colName=filterModelEntry.getKey();
+                FilterModel filterModel=filterModelEntry.getValue();
+                System.out.println("colName in filter : "+colName);
+                /*if(filterModel.getType().equalsIgnoreCase("NUMBER") && filterModel.getFilterType().equalsIgnoreCase("GREATETHEN")){
+                    predicateList.add(criteriaBuilder.ge(companyRoot.get(colName),Long.parseLong(filterModel.getFilter())));
+                }
+                else if(filterModel.getType().equalsIgnoreCase("NUMBER") && filterModel.getFilterType().equalsIgnoreCase("LESSTHEN")){
+                    predicateList.add(criteriaBuilder.lessThan(companyRoot.get(colName),Long.parseLong(filterModel.getFilter())));
+
+                }*/
+                if(filterModel.getType().equalsIgnoreCase("equals")){
+                    predicateList.add(criteriaBuilder.equal(companyRoot.get(colName),filterModel.getFilter()));
+                }
+                else if(filterModel.getType().equalsIgnoreCase("notEqual")){
+                    predicateList.add(criteriaBuilder.notEqual(companyRoot.get(colName),filterModel.getFilter()));
+
+                }
+                else if(filterModel.getType().equalsIgnoreCase("contains")){
+                    predicateList.add(criteriaBuilder.like(companyRoot.get(colName),filterModel.getFilter()));
+
+                }
+                else if(filterModel.getType().equalsIgnoreCase("notContains")){
+                    predicateList.add(criteriaBuilder.notLike(companyRoot.get(colName),filterModel.getFilter()));
+
+                }
+                else if(filterModel.getType().equalsIgnoreCase("startsWith")){
+                    predicateList.add(criteriaBuilder.like(companyRoot.get(colName),"%"+filterModel.getFilter()));
+
+                }
+                else if(filterModel.getType().equalsIgnoreCase("endsWith")){
+                    predicateList.add(criteriaBuilder.like(companyRoot.get(colName),filterModel.getFilter()+"%"));
+                }
+                else if(filterModel.getType().equalsIgnoreCase("blank")){
+                    predicateList.add(criteriaBuilder.isNull(companyRoot.get(colName)));
+                }
+                else if(filterModel.getType().equalsIgnoreCase("notBlank")){
+                    predicateList.add(criteriaBuilder.isNotNull(companyRoot.get(colName)));
+                }
+            }
+        });
+        return  predicateList;
     }
 }
